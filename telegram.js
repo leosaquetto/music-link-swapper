c89986a8b44b3b1da196cd37216239592441024e
leosaquetto <53850390+leosaquetto@users.noreply.github.com>
2026-04-09 20:29:04 -0300

    Add files via upload

---
+++ telegram.js
@@ -0,0 +1,16 @@
+const tg = window.Telegram?.WebApp;
+
+if (tg) {
+  tg.ready();
+  tg.expand();
+
+  try {
+    tg.setHeaderColor("#0f0f10");
+    tg.setBackgroundColor("#0b0b0d");
+  } catch (_error) {}
+
+  document.documentElement.style.setProperty(
+    "--tg-viewport-height",
+    `${tg.viewportHeight || window.innerHeight}px`
+  );
+}
